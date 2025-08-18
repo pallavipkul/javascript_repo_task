671f5fd385b50d840db9e0ae542e4dacce0b3b2c
kulkarni pallavi <kulkarnipallavi2709@gmail.com>
2025-08-18 18:09:28 +0530

    mini_project mini project

---
+++ color_picker-08-08/task08-08.js
@@ -0,0 +1,16 @@
+
+document.body.style.backgroundColor="pink";
+
+function red_color(){
+    document.body.style.backgroundColor="red";
+}
+
+function blue_color(){
+    document.body.style.backgroundColor="blue";
+}
+function orange_color(){
+    document.body.style.backgroundColor="orange";
+}
+function green_color(){
+    document.body.style.backgroundColor="green";
+}

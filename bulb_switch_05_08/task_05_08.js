805d901632bf5f7de3ad530c00faac0ce9996b8f
kulkarni pallavi <kulkarnipallavi2709@gmail.com>
2025-08-18 06:03:06 +0530

    MINI project 1

---
+++ bulb_switch_05_08/task_05_08.js
@@ -0,0 +1,11 @@
+
+document.getElementById("heading").style.color="blue";
+
+
+function switch_on() {
+      document.getElementById("bulb").src ="https://png.pngtree.com/png-vector/20241017/ourmid/pngtree-glowing-light-bulb-clipart-perfect-for-innovation-and-energy-designs-png-image_14107782.png";
+    }
+
+function switch_off() {
+      document.getElementById("bulb").src ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwPaWmyFgIHSNqqIkDuoXU4WG8viYgK_hOWA&s";
+    }

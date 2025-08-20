84b03595593e7201e17f574531bc64237542cbf7
kulkarni pallavi <kulkarnipallavi2709@gmail.com>
2025-08-20 16:59:31 +0530

    Add files via upload

---
+++ user_verification_08_08/script.js
@@ -0,0 +1,26 @@
+// document.getElementById("heading").style.color = "orange";
+
+// function onHandelSubmit() {
+//     let user_name = document.getElementById("name").value;
+//     console.log(user_name);
+//     document.getElementById("message").textContent =
+//         user_name + " your account is verifying......";
+// }
+
+
+function onHandelSubmit(){
+    let user_name=document.getElementById("name").value 
+    console.log(user_name)
+    document.getElementById("message").textContent=user_name + "your account is verifying......";
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ user_verification_08_08/tempCodeRunnerFile.js
@@ -0,0 +1,2 @@
+let n=[1,2,3,4,3,2].map(i => i%2 == 0);
+// console.log(n);
